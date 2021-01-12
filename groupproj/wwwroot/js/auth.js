2fcf3cd719d4ed2ebe682364b1852fc5b7452f36
Fonzy <fzypogi0508@gmail.com>
2021-01-12 16:15:54 +0800

    Add project files.

---
+++ groupproj/wwwroot/js/auth.js
@@ -0,0 +1,153 @@
+﻿var LIF = document.getElementById('LIF');
+var LIFs = document.getElementById('Lifs');
+var acc = document.getElementById('emailzy');
+var userID;
+var ACC_navbar = document.getElementById('accFzy');
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        userID = user.uid;
+        console.log(userID);
+        ACC_navbar.style.display = "block";
+        //emailinfo
+        db.collection('users').doc(user.uid).collection('guides').onSnapshot(snapshot => {
+            setupGuides(snapshot.docs);
+
+            const html = `
+            <p class="sizes optionL">Email account: ${user.email}</p>
+            `;
+            acc.innerHTML = html;
+        });
+       
+       
+        LIFs.style.display = "block";
+        console.log('userlogged in', user);
+        LIF.style.display = "none";
+        document.getElementById('logoutbtn').style.visibility = "visible";
+        document.getElementById('loginbtn').style.visibility = "hidden";
+       
+
+    } else {
+
+        console.log('user logged out');
+        LIF.style.display = "block";
+        LIFs.style.display = "none";
+        setupGuides([])
+        ACC_navbar.style.display = "none";
+    }
+
+});
+
+const guidelist = document.querySelector('.guides');
+//setup guides
+const setupGuides = (data) => {
+
+    let html = '';
+    data.forEach(doc => {
+        const guide = doc.data();
+        const li = `
+            <li style="list-style:none;">
+                <div class = "ttl">${guide.title}</div >
+                <div class="cnt"> ${guide.time} </div> 
+               </li >
+            `;
+        html += li
+    });
+    guidelist.innerHTML = html;
+}
+
+
+
+function AddR() {
+    var hr = document.getElementById('add-RCH').value;
+    var mn = document.getElementById('add-RCM').value ;
+    var sc = document.getElementById('add-RCS').value;
+    var time;
+
+       if (hr == "Hour" || hr == "") {
+            console.log("no hours inserted.");
+            hr = "";
+        } else {
+            hr = hr + " Hour/s ";
+        }
+        if (mn == "Mins" || mn == "") {
+            console.log("no minute inserted.");
+            mn = "";
+        } else {
+            mn = mn + " Min/s ";
+        }
+        if (sc == "Secs" || sc == "") {
+            console.log("no second inserted.");
+            sc = "";
+        } else {
+            sc = sc + " Sec/s ";
+        }
+
+   Time = "Every " + hr + mn + sc;
+
+    db.collection('users').doc(userID).collection('guides').add({
+        title: document.getElementById('add-RT').value,
+        time: Time
+    });
+
+    document.getElementById('add-RT').value = "";
+    document.getElementById('add-RCH').value = "Hour";
+    document.getElementById('add-RCM').value = "Mins";
+    document.getElementById('add-RCS').value = "Secs";
+    document.getElementById('addReminder').style.display = "none";
+}
+
+function signup() {
+    const email = document.getElementById('SU-EMAILinpt').value;
+    const password = document.getElementById('SU-PASSinpt').value;
+
+    console.log(email, password);
+    //sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        return db.collection('users').doc(cred.user.uid).set({
+            email: document.getElementById('SU-EMAILinpt').value
+        })
+        
+    }).then(() => {
+        console.log(cred.user);
+        credzy = cred.user.uid;
+    });
+    document.getElementById('signup-con').style.visibility = "hidden";
+    document.getElementById('blur').style.visibility = "hidden";
+    document.getElementById('logoutbtn').style.visibility = "visible";
+    document.getElementById('loginbtn').style.visibility = "hidden";
+    document.getElementById('closebtn').style.visibility = "visible";
+}
+
+//LOGOUT
+function logoutNW() {
+    auth.signOut();
+    document.getElementById('logoutbtn').style.visibility = "hidden";
+    document.getElementById('loginbtn').style.visibility = "visible";
+    document.getElementById('closebtn').style.visibility = "hidden";
+    document.getElementById('SU-EMAILinpt').value = ' '
+    document.getElementById('SU-PASSinpt').value = ' '
+    credzy = "";
+    window.location.reload(); 
+}
+
+//login
+function loginNW() {
+    const email = document.getElementById('EMAILinpt').value;
+    const password = document.getElementById('PASSinpt').value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        credzy = cred.user.uid;
+    })
+    document.getElementById('EMAILinpt').value = ' ';
+    document.getElementById('PASSinpt').value = ' ';
+    document.getElementById('login').style.visibility = "hidden";
+    document.getElementById('blur').style.visibility = "hidden";
+    document.getElementById('logoutbtn').style.visibility = "visible";
+    document.getElementById('loginbtn').style.visibility = "hidden";
+    document.getElementById('closebtn').style.visibility = "hidden";
+    
+
+}
+
---
+++ groupproj/wwwroot/js/site.js
@@ -0,0 +1,180 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+
+
+// Write your Javascript code.
+
+function loginopen() {
+    document.getElementById('login').style.visibility = "visible";
+    document.getElementById('blur').style.visibility = "visible";
+    document.getElementById('closebtn').style.visibility = "visible";
+}
+function loginclose() {
+    document.getElementById('login').style.visibility = "hidden";
+    document.getElementById('blur').style.visibility = "hidden";
+    document.getElementById('closebtn').style.visibility = "hidden";
+}
+function signupclc() {
+    document.getElementById('login').style.display = "none";
+    document.getElementById('signup-con').style.visibility = "visible ";
+    document.getElementById('signup-con').style.display = "block ";
+    document.getElementById('closebtn1').style.visibility = "visible";
+    
+}
+function accdetclc() {
+    document.getElementById('login').style.display = "none";
+    document.getElementById('Accdetails').style.visibility = "visible ";
+    document.getElementById('Accdetails').style.display = "block ";
+    document.getElementById('closebtn2').style.visibility = "visible";
+    
+}
+
+function addRclc() {
+    document.getElementById('login').style.display = "none";
+    document.getElementById('addReminder').style.visibility = "visible ";
+    document.getElementById('addReminder').style.display = "block ";
+    document.getElementById('closebtn2').style.visibility = "visible";
+    
+}
+
+function reminderopen() {
+    document.getElementById('timmerremindercon').style.display = "block";
+    document.getElementById('timmerremindercon').style.right = "0";
+    document.getElementById('fzy').style.visibility = "hidden";
+    
+}
+function closeRemind() {
+    document.getElementById('timmerremindercon').style.right = "-401px";
+    document.getElementById('timmerremindercon').style.animationDuration = "2s";
+    document.getElementById('fzy').style.visibility = "visible";
+}
+function closezybtn() {
+    document.getElementById('login').style.visibility = "hidden";
+    document.getElementById('signup-con').style.visibility = "hidden";
+    
+    document.getElementById('addReminder').style.visibility = "hidden";
+    document.getElementById('blur').style.visibility = "hidden";
+    document.getElementById('closebtn').style.visibility = "hidden";
+    document.getElementById('closebtn1').style.visibility = "hidden";
+    document.getElementById('closebtn2').style.visibility = "hidden";
+    document.getElementById('SU-EMAILinpt').value = '';
+    document.getElementById('SU-PASSinpt').value = '';
+    document.getElementById('EMAILinpt').value = '';
+    document.getElementById('PASSinpt').value = '';
+}
+function dropdowninfo() {
+    document.getElementById('txtinfo').style.top = "430px";
+    document.getElementById('about-button').style.visibility = "hidden";
+    document.getElementById('fzyI').style.paddingTop = "10px";
+}
+function dropdowninfoclose() {
+    document.getElementById('txtinfo').style.top = "100px";
+    document.getElementById('about-button').style.visibility = "visible";
+}
+function dropFAQ() {
+    document.getElementById('infoFAQ').style.top = "150px";
+    document.getElementById('btn1').style.visibility = "hidden";
+/*hidebtn2*/
+    document.getElementById('btn2').style.top = "270px";
+    document.getElementById('zyzy').style.top = "270px";
+    document.getElementById('btn3').style.top = "315px";
+    document.getElementById('sbtn3').style.top = "315px";
+    document.getElementById('infoFAQ2').style.top = "-100px";
+    document.getElementById('infoFAQ3').style.top = "-200px";
+    document.getElementById('btn2').style.visibility = "visible";
+    document.getElementById('btn3').style.visibility = "visible";
+    document.getElementById('sbtn3').style.visibility = "hidden";
+}
+function closeFAQ() {
+    document.getElementById('infoFAQ').style.top = "20px";
+    document.getElementById('btn1').style.visibility = "visible";
+    document.getElementById('btn2').style.top = "150px";
+    document.getElementById('btn3').style.top = "190px";
+    document.getElementById('sbtn3').style.top = "190px";
+    document.getElementById('zyzy').style.top = "150px";
+}
+function dropFAQ2() {
+    document.getElementById('infoFAQ2').style.top = "190px";
+    document.getElementById('btn2').style.visibility = "hidden";
+    document.getElementById('btn3').style.top = "440px";
+    document.getElementById('sbtn3').style.top = "440px";
+    /*hidebtn1*/
+    document.getElementById('zyzy').style.top = "150px";
+    document.getElementById('btn2').style.top = "150px";
+    document.getElementById('infoFAQ').style.top = "20px";
+    document.getElementById('infoFAQ3').style.top = "-200px";
+    document.getElementById('btn1').style.visibility = "visible";
+    document.getElementById('btn3').style.visibility = "visible";
+    document.getElementById('sbtn3').style.visibility = "hidden";
+}
+function closeFAQ2() {
+    document.getElementById('infoFAQ2').style.top = "-100px";
+    document.getElementById('btn2').style.visibility = "visible";
+    document.getElementById('btn3').style.top = "190px";
+    document.getElementById('sbtn3').style.top = "190px";
+}
+function dropFAQ3() {
+    document.getElementById('infoFAQ3').style.top = "255px";
+    document.getElementById('infoFAQ2').style.top = "-100px";
+    document.getElementById('infoFAQ').style.top = "20px";
+    document.getElementById('btn3').style.visibility = "hidden";
+    document.getElementById('btn3').style.top = "190px";
+    document.getElementById('btn2').style.top = "150px";
+    document.getElementById('zyzy').style.top = "150px";
+    document.getElementById('sbtn3').style.top = "190px";
+    document.getElementById('sbtn3').style.visibility = "visible";
+    document.getElementById('sbtn3').style.borderBottomLeftRadius = "0px";
+
+}
+function closeFAQ3() {
+    document.getElementById('infoFAQ3').style.top = "-300px";
+    document.getElementById('btn3').style.visibility = "visible";
+    document.getElementById('sbtn3').style.visibility = "hidden";
+
+}
+var input = "info here";
+
+function getinfo1() {
+    input = document.getElementById('inp1').value;
+    document.getElementById('cng').innerHTML = input;
+}
+
+function SHWFAQ1() {
+    document.getElementById('XFAQ').style.visibility = "visible";
+    document.getElementById('XFAQ').style.right = "395px";
+    document.getElementById('infoFAQ3').style.right = "0px";
+    document.getElementById('infoFAQ2').style.right = "0px";
+    document.getElementById('infoFAQ').style.right = "0px";
+    document.getElementById('FAQ').style.right = "0px";
+    document.getElementById('btn1').style.right = "0px";
+    document.getElementById('sbtn1').style.right = "0px";
+    document.getElementById('btn2').style.right = "0px";
+    document.getElementById('zyzy').style.right = "0px";
+    document.getElementById('btn3').style.right = "0px";
+    document.getElementById('sbtn3').style.right = "0px"; 
+    document.getElementById('hiderr').style.right = "0px"; 
+}
+function HDEFAQ1() {
+    document.getElementById('XFAQ').style.right = "-420px";
+    document.getElementById('infoFAQ').style.right = "-420px";
+    document.getElementById('infoFAQ3').style.right = "-420px";
+    document.getElementById('infoFAQ2').style.right = "-420px";
+    document.getElementById('FAQ').style.right = "-420px";
+    document.getElementById('btn1').style.right = "-420px";
+    document.getElementById('sbtn1').style.right = "-420px";
+    document.getElementById('btn2').style.right = "-420px";
+    document.getElementById('zyzy').style.right = "-420px";
+    document.getElementById('btn3').style.right = "-420px";
+    document.getElementById('sbtn3').style.right = "-420px";
+    document.getElementById('hiderr').style.right = "-420px"; 
+
+}
+function CNTCLS1() {
+    document.getElementById('CLP').style.display = "none";
+
+}
+
+// auth 
+
+
